c8e92d476d13fc78d1d4049f53b648ef4848cb73
Shelby Gleason <shelbygleason@Shelbys-MacBook-Air.local>
2021-09-23 14:01:51 -0600

    fix

--- cart.js
+++ cart.js
@@ -1,3 +1,17 @@
 <body onload="doShowAll()">
 
 window.load=doShowAll();
+
+/*
+=====> Checking browser support.
+ //This step might not be required because most modern browsers do support HTML5.
+ */
+ //Function below might be redundant.
+function CheckBrowser() {
+    if ('localStorage' in window && window['localStorage'] !== null) {
+        // We can use localStorage object to store data.
+        return true;
+    } else {
+            return false;
+    }
+}
